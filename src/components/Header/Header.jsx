668bdfd081d61fbedf3fe3ed8b45022adc41838e
Saleh545 <syusifzade545@gmail.com>
2024-08-07 17:16:55 +0400

    first

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,130 @@
-import React from 'react'
-import logo from "../../assets/icons/ty-web.svg"
+import React from "react";
+import logo from "../../assets/icons/ty-web.svg";
+import { Link, NavLink } from "react-router-dom";
+import { IoIosSearch } from "react-icons/io";
+import { FaRegUser } from "react-icons/fa";
+import { CiHeart } from "react-icons/ci";
+import { SlBasket } from "react-icons/sl";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+
+
+
+
 const Header = () => {
   return (
+    <div>
+      <div className="container">
+        <div id="headerMain">
+          <div className="header-top">
+            <ul className="right">
+              <li>
+                <NavLink>İndirim Kuponlarım</NavLink>
+              </li>
+              <li>
+                <NavLink>Trendyol'da Satış Yap</NavLink>
+              </li>
+              <li>
+                <NavLink>Hakkımızda</NavLink>
+              </li>
+              <li>
+                <NavLink>Yardım & Destek</NavLink>
+              </li>
+            </ul>
+          </div>
 
-    <div className="fd">
-<div className="container">
+          <div className="header">
+            <div>
+              <NavLink to="/">
+                <img src={logo} id="logo" alt="" />
+              </NavLink>
+            </div>
+            <div className="mOkuL__p">
+              <div className="N4M8bfaJ">
+                <input
+                  type="text"
+                  className="V8wbcUhU"
+                  placeholder="Aradığınız ürün, kategori veya markayı yazınız"
+                />
+                <IoIosSearch className="cyrzo7gC" />
+              </div>
+            </div>
 
-    <div className='hea'><img src={logo} alt="" /></div>
-</div>
+            <div className="account-navigation-wrapper">
+              <div className="account-nav-item user-login-container">
+                <div className="link account-user">
+                  <FaRegUser />
+                  <p className="link-text">Giriş Yap</p>
+                </div>
+                <div className="new-login-dropdown">
+                  <div className="user-notloggedin-container container-padding">
+                    <div class="login-button">Giriş Yap</div>
+                    <div class="signup-button signup-button-container">
+                      Üye Ol
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <NavLink className="account-nav-item account-favorites">
+                <div className="link">
+                  <CiHeart className="initial-icon" />
+                  <p class="link-text">Favorilerim</p>
+                </div>
+              </NavLink>
+              <div class="account-nav-item basket-preview">
+                <NavLink className="link account-basket">
+                  <SlBasket />
+                  <p class="link-text">Sepetim</p>
+                </NavLink>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="container">
+        <div id="navigation-wrapper">
+<nav>
+  <ul className="main-nav">
+    <li className="tab-link">
+      <NavLink className="category-header navbar-first-cap">Kadın</NavLink>
+    </li>
+    <li className="tab-link">
+      <NavLink className="category-header navbar-first-cap">Erkek</NavLink>
+    </li>
+    <li className="tab-link">
+      <NavLink className="category-header navbar-first-cap">Anne & Çocuk</NavLink>
+    </li>
+    <li className="tab-link">
+      <NavLink className="category-header navbar-first-cap">Ev & Yaşam</NavLink>
+    </li>
+    <li className="tab-link">
+      <NavLink className="category-header navbar-first-cap">Süpermarket</NavLink>
+    </li>
+    <li className="tab-link">
+      <NavLink className="category-header navbar-first-cap">Kozmetik</NavLink>
+    </li>
+    <li className="tab-link">
+      <NavLink className="category-header navbar-first-cap">Ayakkabı & Çanta</NavLink>
+    </li>
+    <li className="tab-link">
+      <NavLink className="category-header navbar-first-cap">Elektronik</NavLink>
+    </li>
+    <li className="tab-link">
+      <NavLink className="category-header navbar-first-cap">Spor&Outdoor</NavLink>
+    </li>
+    <li className="tab-link">
+      <NavLink className="category-header navbar-first-cap">Çok Satanlar <span class="new-badge">Yeni</span></NavLink>
+    </li>
+    <li className="tab-link">
+      <NavLink className="category-header navbar-first-cap">Flaş Ürünler <span class="new-badge">Yeni</span></NavLink>
+    </li>
+  </ul>
+</nav>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,14 +1,154 @@
-import React from 'react'
-import Header from '../../components/Header/Header.jsx'
+import React from 'react';
+import Header from '../../components/Header/Header.jsx';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+import { Link } from "react-router-dom";
+import sen from "../../assets/images/sen.png";
+import sana from "../../assets/images/sana.png";
+import destek from "../../assets/images/destek.png";
+import deprem from "../../assets/images/deprem.png";
+import kurumsal from "../../assets/images/kurumsal.png";
+import kredit from "../../assets/images/kredit.png";
+import kredit2 from "../../assets/images/kredit2.png";
+import apple from "../../assets/images/apple.png";
+import philips from "../../assets/images/philips.png";
+import pull from "../../assets/images/pull.png";
+import samsung from "../../assets/images/samsung.png";
+
+let settings = {
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 11,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 1500,
+  pauseOnHover: true, 
+  responsive: [
+    {
+      breakpoint: 1024, // Büyük ekranlar için
+      settings: {
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 768, // Orta boy ekranlar için
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 480, // Küçük ekranlar için
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    }
+  ]
+};
 
 const Home = () => {
   return (
     <div>
-    <Header/>
-    
-    
-    </div>
-  )
-}
+      <Header />
+      <div className="homepage-wrapper">
+        <div className="container">
+          <div className="slider-container">
+            <Slider {...settings}>
+              <div>
+                <Link to="/" className="item">
+                  <img loading="lazy" src={sen} alt="Sen De Al!" />
+                  <span>Sen De Al!</span>
+                </Link>
+              </div>
+              <div>
+                <Link to="/" className="item">
+                  <img loading="lazy" src={sana} alt="Sana Özel" />
+                  <span>Sana Özel</span>
+                </Link>
+              </div>
+              <div>
+                <Link to="/" className="item">
+                  <img loading="lazy" src={destek} alt="Dijital Destek Kartları" />
+                  <span>Dijital Destek Kartları</span>
+                </Link>
+              </div>
+              <div>
+                <Link to="/" className="item">
+                  <img loading="lazy" src={deprem} alt="Deprem Yardımı" />
+                  <span>Deprem Yardımı</span>
+                </Link>
+              </div>
+              <div>
+                <Link to="/" className="item">
+                  <img loading="lazy" src={kurumsal} alt="Kurumsal" />
+                  <span>Kurumsal</span>
+                </Link>
+              </div>
+              <div>
+                <Link to="/" className="item">
+                  <img loading="lazy" src={kredit} alt="Kredi Kartı" />
+                  <span>Kredi Kartı</span>
+                </Link>
+              </div>
+              <div>
+                <Link to="/" className="item">
+                  <img loading="lazy" src={kredit2} alt="Krediler" />
+                  <span>Krediler</span>
+                </Link>
+              </div>
+              <div>
+                <Link to="/" className="item">
+                  <img loading="lazy" src={apple} alt="Apple" />
+                  <span>Apple</span>
+                </Link>
+              </div>
+              <div>
+                <Link to="/" className="item">
+                  <img loading="lazy" src={philips} alt="Philips" />
+                  <span>Philips</span>
+                </Link>
+              </div>
+              <div>
+                <Link to="/" className="item">
+                  <img loading="lazy" src={pull} alt="Pull & Bear" />
+                  <span>Pull & Bear</span>
+                </Link>
+              </div>
+              <div>
+                <Link to="/" className="item">
+                  <img loading="lazy" src={samsung} alt="Samsung" />
+                  <span>Samsung</span>
+                </Link>
+              </div>
+            </Slider>
+          </div>
 
-export default Home
+          <div class="widget-container no-padding extended-container">
+            <div className="banner-listing-items">
+              <Link>
+              <img alt="Sepete En Çok Eklenenler" class="p-card-img " src="https://cdn.dsmcdn.com/mnresize/500/500/marketing/datascience/automation/2023/4/18/Sepet_202304181225.png"></img>
+              </Link>
+
+              <Link>
+              <img alt="En Çok Öne Çıkanlar" class="p-card-img " src="https://cdn.dsmcdn.com/mnresize/500/500/marketing/datascience/automation/2023/4/18/Onecikan_202304181225.png"></img>
+              </Link>
+              <Link>
+              <img alt="Flaş Ürünler" class="p-card-img " src="https://cdn.dsmcdn.com/mnresize/500/500/marketing/datascience/automation/2023/4/18/Flash_202304181225.png"></img>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
